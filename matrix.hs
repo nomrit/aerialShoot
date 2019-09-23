e38845d44c18abe7149af9705f419856f0a87b76
nomrit <Ritsu.Nomura@gmail.com>
2019-09-23 11:40:44 +0900

    スカラー差と行列式を追加。

--- matrix.hs
+++ matrix.hs
@@ -1,5 +1,5 @@
 module Matrix
-       ( toR, toD, dot, (|.|), cross, (|*|), (|+|), sc, (/*/) )
+       ( toR, toD, dot, (|.|), cross, (|*|), (|+|), (|-|), sc, (/*/), det )
        where
 ---- 一般
 toR::Double -> Double
@@ -28,9 +28,20 @@ x |*| y = cross x y
 (|+|) :: [Double] -> [Double] -> [Double]
 x |+| y = zipWith (+) x y
 
+-- 差
+(|-|) :: [Double] -> [Double] -> [Double]
+x |-| y = zipWith (-) x y
+
 -- スカラー積
 sc :: Double -> [Double] -> [Double]
 sc x y = map (* x) y
 
 (/*/) :: Double -> [Double] -> [Double]
 x /*/ y = sc x y
+
+-- 行列式(3x3まで)
+det :: [[Double]] -> Double
+det [[a]] = a
+det [[a,b],[c,d]] = a*d - b*c
+det [[a1,a2,a3],[a4,a5,a6],[a7,a8,a9]] = a1*a5*a9+a2*a6*a7+a3*a4*a8-a3*a5*a7-a2*a4*a9-a1*a6*a8
+
