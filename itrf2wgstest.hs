6da2b6e4e297a29d82e18e6866b67b9877264599
nomrit <Ritsu.Nomura@gmail.com>
2020-04-26 10:57:48 +0900

    ITRF試験用プログラムを追加

---
+++ itrf2wgstest.hs
@@ -0,0 +1,25 @@
+-- itrf2wgstest
+--
+import System.IO
+import Data.List.Split
+import ITRF
+
+list2tuple::[Double] -> (Int,Double,Double)
+list2tuple [z,x,y] = (floor(z),x,y)
+
+round7 :: Double -> Double
+round7 x = (fromIntegral(round (x*10^7))::Double)/10^7
+
+main = do
+       mainloop stdin stdout
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop inh outh =
+    do ineof <- hIsEOF inh
+       if ineof
+          then return ()
+          else do inStr <- hGetLine inh
+                  if (head inStr == '#')
+                     then hPutStrLn outh inStr
+                     else hPutStrLn outh ("("++inStr ++ ")," ++ (show (itrf2wgs (map (read::String->Double) (splitOn "," inStr)))))
+                  mainloop inh outh
